8dab77d49cefeba702567620ca6ff23c8ed87e91
tynchtyk1985 <turganbekov1985@mail.ru>
2024-02-11 15:53:55 +0600

    lesson_09.02-task10

---
+++ task10/Program.cs
@@ -0,0 +1,19 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+
+int produkt1 = i1 * i2;
+long produkt2 = l1 * i1;
+double produkt3 = i1 * d1;
+Console.WriteLine(produkt1);
+Console.WriteLine(produkt2);
+Console.WriteLine(produkt3);
